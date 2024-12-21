93d76269f5b54862eba2e9333739cb93acbc3304
nataliafatkina <fatkina.natali@gmail.com>
2024-12-21 08:46:27 +0300

    add task3

---
+++ UrbanDjango/task2/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+def main_page(request):
+    return render(request, 'second_task/func_template.html')
+
+class First_page(TemplateView):
+    template_name = 'second_task/class_template.html'
